91dd5713229d64115da4ac5f5efad146c6488548
Michele Mostarda <michele.mostarda@gmail.com>
2012-08-17 16:18:53 +0200

    Resetting confidence to avoid test variance.

--- src/test/java/com/machinelinking/enricher/WikiEnricherTest.java
+++ src/test/java/com/machinelinking/enricher/WikiEnricherTest.java
@@ -6,6 +6,7 @@ import com.machinelinking.serializer.JSONSerializer;
 import com.machinelinking.util.JSONUtils;
 import junit.framework.Assert;
 import org.codehaus.jackson.JsonNode;
+import org.codehaus.jackson.node.ObjectNode;
 import org.junit.Test;
 import org.xml.sax.SAXException;
 
@@ -45,7 +46,14 @@ public class WikiEnricherTest {
                 this.getClass().getResourceAsStream("/Enrichment.json")
         );
         final JsonNode extractedJSON = JSONUtils.parseJSON(baos.toString());
+        resetConfidence(expectedJSON);
+        resetConfidence(extractedJSON);
         Assert.assertTrue( expectedJSON.equals(extractedJSON) );
+    }
+
+    private void resetConfidence(JsonNode node) {
+        final ObjectNode onode = (ObjectNode) node.get("freebase");
+        onode.put("relevance:score", 0);
     }
 
 }
