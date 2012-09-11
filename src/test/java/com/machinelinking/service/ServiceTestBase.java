5c72cdfed9a6b87bfabe1a020806c21432cdc875
Michele Mostarda <michele.mostarda@gmail.com>
2012-09-11 15:42:13 +0200

    Fixed issue with service resources mime types.

--- src/test/java/com/machinelinking/service/ServiceTestBase.java
+++ src/test/java/com/machinelinking/service/ServiceTestBase.java
@@ -4,6 +4,8 @@ import com.sun.jersey.api.container.grizzly2.GrizzlyServerFactory;
 import com.sun.jersey.api.core.PackagesResourceConfig;
 import com.sun.jersey.api.core.ResourceConfig;
 import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.grizzly.http.server.Request;
+import org.glassfish.grizzly.http.server.Response;
 import org.glassfish.grizzly.http.server.StaticHttpHandler;
 import org.junit.After;
 import org.junit.Before;
@@ -21,12 +23,14 @@ public class ServiceTestBase {
     public static final String HOST = "localhost";
     public static final int PORT    = 9998;
 
+    private static final String CONTENT_TYPE = "Content-Type";
+
     public static final URI BASE_URI = getBaseURI().build();
 
     private HttpServer httpServer;
 
     protected static UriBuilder getBaseURI() {
-        return UriBuilder.fromUri( String.format("http://%s", HOST)).port(PORT);
+        return UriBuilder.fromUri( String.format("http://%s/", HOST)).port(PORT);
     }
 
     @Before
@@ -35,8 +39,18 @@ public class ServiceTestBase {
         ResourceConfig rc = new PackagesResourceConfig(ServiceTestBase.class.getPackage().getName());
         httpServer = GrizzlyServerFactory.createHttpServer(BASE_URI, rc);
         httpServer.getServerConfiguration().addHttpHandler(
-                new StaticHttpHandler("./src/main/resources/frontend/"),
-                "/frontend"
+                new StaticHttpHandler("./src/main/resources/frontend/"){
+                    @Override
+                    protected boolean handle(String uri, Request req, Response res) throws Exception {
+                        if(uri.endsWith(".css")) {
+                            res.setHeader(CONTENT_TYPE, "text/css");
+                        } else if(uri.endsWith(".js")) {
+                            res.setHeader(CONTENT_TYPE, "test/javascript");
+                        }
+                        return super.handle(uri, req, res);
+                    }
+                },
+                "/frontend/"
         );
         httpServer.start();
 
