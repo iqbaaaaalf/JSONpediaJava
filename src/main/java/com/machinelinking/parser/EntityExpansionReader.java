34bde662fc0f769dbdd25e3a0f6a84510a147c54
Michele Mostarda <michele.mostarda@gmail.com>
2013-06-18 22:22:52 +0200

    Added first version of EntityExpansionReader.java and test case.

---
+++ src/main/java/com/machinelinking/parser/EntityExpansionReader.java
@@ -0,0 +1,106 @@
+package com.machinelinking.parser;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.Reader;
+
+/**
+ * @author Michele Mostarda (mostarda@fbk.eu)
+ */
+public class EntityExpansionReader extends Reader {
+
+    private final short MAX_ENTITY_NUMERIC_SIZE = 6;
+
+    private final BufferedReader inner;
+
+    private final StringBuilder intBuffer = new StringBuilder();
+
+    @Override
+    public void mark(int readAheadLimit) throws IOException {
+        inner.mark(readAheadLimit);
+    }
+
+    @Override
+    public void reset() throws IOException {
+        inner.reset();
+    }
+
+    public EntityExpansionReader(BufferedReader inner) {
+        this.inner = inner;
+    }
+
+    @Override
+    public int read(char[] cbuf, int off, int len) throws IOException {
+        int intc;
+        char c;
+        int charsRead = 0;
+        boolean foundTerminal = false;
+        int i;
+        for (i = off; i < len; i++) {
+            intc = inner.read();
+            if (intc == -1) break;
+            c = (char) intc;
+            if (c == '&') {
+                inner.mark(MAX_ENTITY_NUMERIC_SIZE + 1);
+                intBuffer.delete(0, intBuffer.length());
+                for(int l = 0; l < MAX_ENTITY_NUMERIC_SIZE; l++) {
+                    intc = inner.read();
+                    if (intc == -1) break;
+                    c = (char) intc;
+                    if (c == ';') {
+                        foundTerminal = true;
+                        break;
+                    }
+                    intBuffer.append(c);
+                }
+                if(foundTerminal && intBuffer.length() > 0) {
+                    final String entitySequence = intBuffer.toString();
+                    final char type = entitySequence.charAt(0);
+                    if(type == '#') { // &#nnnn; OR &#xhhhh;
+                        if (entitySequence.length() > 1) {
+                            if (entitySequence.charAt(1) == 'x') {
+                                cbuf[i] = (char) Long.parseLong(entitySequence.substring(2), 16);
+                            } else {
+                                cbuf[i] = (char) Integer.parseInt(entitySequence.substring(1));
+                            }
+                            charsRead++;
+                        } else {
+                            i--;
+                        }
+                    } else { // &aaaa;
+                        final int bufferLength = intBuffer.length();
+                        cbuf[i++] = '&';
+                        int k;
+                        for(k = 0; k < bufferLength; k++) cbuf[i+k] = intBuffer.charAt(k);
+                        cbuf[k+i] = ';';
+                        charsRead += bufferLength + 2;
+                        i += bufferLength;
+                    }
+                } else { // &something else...
+                    cbuf[i++] = '&';
+                    final int bufferLength = intBuffer.length();
+                    if (!foundTerminal && bufferLength > 0) {
+                        int k;
+                        for (k = 0; k < bufferLength; k++) cbuf[i + k] = intBuffer.charAt(k);
+                        charsRead += bufferLength + 1;
+                        i = k + 1;
+                    }
+                }
+            } else {
+                cbuf[i] = c;
+                charsRead++;
+            }
+        }
+        return charsRead == 0 ? -1 : charsRead;
+    }
+
+    @Override
+    public void close() throws IOException {
+        this.inner.close();
+    }
+
+    private boolean isHexLetter(char c) {
+        return ('a' <= c && c <= 'f') ||  ('A' <= c && c <= 'F');
+    }
+
+}
---
+++ src/test/java/com/machinelinking/parser/EntityExpansionReaderTest.java
@@ -0,0 +1,81 @@
+package com.machinelinking.parser;
+
+import junit.framework.Assert;
+import org.junit.Test;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.StringReader;
+
+/**
+ * @author Michele Mostarda (mostarda@fbk.eu)
+ */
+public class EntityExpansionReaderTest {
+
+    @Test
+    public void testNoExpansion() throws IOException {
+        checkString("abc def", "abc def");
+    }
+
+    @Test
+    public void testEmpty() throws IOException {
+        checkString(">&#;<", "><");
+    }
+
+    @Test
+    public void testMissingEnd() throws IOException {
+        checkString("&#1234", "&#1234");
+    }
+
+    @Test
+    public void testExpansionWithNumer() throws IOException {
+        checkString(">&#1234;<", ">Ӓ<");
+    }
+
+    @Test
+    public void testExpansionWithHex1() throws IOException {
+        checkString(">&#x7c;<", ">|<");
+    }
+
+    @Test
+    public void testExpansionWithHex2() throws IOException {
+        checkString(">&#x70;<", ">p<");
+    }
+
+    @Test
+    public void testMultiExpansion() throws IOException {
+        final String IN = "pre1>&#91;<post1 pre2>&91;<post2 pre3>&#91;&#x5D;<post3";
+        final String EXPECTED = IN.replaceAll("&#91;", "[").replaceAll("&#x5D;", "]");
+        checkString(IN, EXPECTED);
+    }
+
+    @Test
+    public void testNamedEntityNoExpansion() throws IOException {
+        checkString(">&nbsp;<", ">&nbsp;<");
+    }
+
+    @Test
+    public void testNeutral() throws IOException {
+        checkString("A&TLincoln.jpg", "A&TLincoln.jpg");
+    }
+
+    @Test
+    public void testRealCase() throws IOException {
+        checkString(
+                "Samuel Augustus Ward &#91;hymnal&#93;:Print Material Full Description",
+                "Samuel Augustus Ward [hymnal]:Print Material Full Description"
+        );
+    }
+
+    public void checkString(String in, String expected) throws IOException {
+        final EntityExpansionReader expandableReader = new EntityExpansionReader( new BufferedReader(new StringReader(in)) );
+        final char[] buffer = new char[expected.length()];
+        final int readChars = expandableReader.read(buffer, 0, buffer.length);
+        final String out = new String(buffer);
+        System.out.printf("Expected: '%s'\n", expected);
+        System.out.printf("Out     : '%s'\n", out);
+        Assert.assertEquals(expected.length() == 0 ?  -1 : expected.length(), readChars);
+        Assert.assertEquals(expected, out);
+    }
+
+}
