1da9a89b65f5b512c3115bd2df60bfe8e3836f1e
Michele Mostarda <michele.mostarda@gmail.com>
2014-03-30 22:10:02 +0200

    Added toString.

--- src/main/java/com/machinelinking/storage/StorageLoaderReport.java
+++ src/main/java/com/machinelinking/storage/StorageLoaderReport.java
@@ -28,4 +28,15 @@ public class StorageLoaderReport {
     public long getElapsedTime() {
         return elapsedTime;
     }
+
+    @Override
+    public String toString() {
+        return String.format(
+                "Processed pages: %d, pages with error: %d, elapsed time: %d ms, pages/ms: %f, errors/total pages: %f",
+                processedPages, pageErrors, elapsedTime,
+                (processedPages / (float)elapsedTime),
+                (pageErrors / (float) processedPages)
+        );
+    }
+
 }
