87bb793f4d2e3d099a415a64a66f0c5123e1f92f
Michele Mostarda <michele.mostarda@gmail.com>
2014-03-31 23:01:51 +0200

    Removed exception list.

--- src/main/java/com/machinelinking/wikimedia/ProcessorReport.java
+++ src/main/java/com/machinelinking/wikimedia/ProcessorReport.java
@@ -1,8 +1,5 @@
 package com.machinelinking.wikimedia;
 
-import java.util.Arrays;
-import java.util.concurrent.ExecutionException;
-
 /**
  * The report of a {@link PageProcessor}.
  *
@@ -13,17 +10,14 @@ public class ProcessorReport {
     private final long processedPages;
     private final long pagesWithError;
     private final long elapsedTime;
-    private final ExecutionException[] executionExceptions;
 
     public ProcessorReport(
             long processedPages, long pagesWithError,
-            long elapsedTime,
-            ExecutionException[] executionExceptions
+            long elapsedTime
     ) {
         this.processedPages = processedPages;
         this.pagesWithError = pagesWithError;
         this.elapsedTime = elapsedTime;
-        this.executionExceptions = executionExceptions;
     }
 
     public long getProcessedPages() {
@@ -36,18 +30,14 @@ public class ProcessorReport {
         return elapsedTime;
     }
 
-    public ExecutionException[] getExecutionExceptions() {
-        return executionExceptions;
-    }
-
     @Override
     public String toString() {
         return String.format(
-                "Processed pages: %d, pages with errors: %d, elapsed time: %d (ms), exceptions: %s",
+                "Processed pages: %d, pages with errors: %d, elapsed time: %ds (%dms)",
                 processedPages,
                 pagesWithError,
                 elapsedTime / 1000,
-                Arrays.asList(executionExceptions)
+                elapsedTime
         );
     }
 
