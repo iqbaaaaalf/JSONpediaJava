efd7004040eb41d35684659ca0c5dc79c9e7686b
Michele Mostarda <michele.mostarda@gmail.com>
2014-03-31 19:20:52 +0200

    Added more consumers and random consumption wait.

--- src/test/java/com/machinelinking/wikimedia/BufferedWikiPageHandlerTest.java
+++ src/test/java/com/machinelinking/wikimedia/BufferedWikiPageHandlerTest.java
@@ -3,6 +3,8 @@ package com.machinelinking.wikimedia;
 import junit.framework.Assert;
 import org.junit.Test;
 
+import java.util.Random;
+
 /**
  * Test case for {@link com.machinelinking.wikimedia.BufferedWikiPageHandler}.
  *
@@ -12,26 +14,34 @@ public class BufferedWikiPageHandlerTest {
 
     @Test
     public void testWriteOneConsumeMulti() throws InterruptedException {
-        for(int i = 0; i < 20; i++) {
+        for(int i = 0; i < 10; i++) {
             writeOneConsumeMulti();
         }
     }
 
     private void writeOneConsumeMulti() throws InterruptedException {
         final BufferedWikiPageHandler wikiPageHandler = new BufferedWikiPageHandler();
-        final int COUNT = 20000;
+        final int COUNT = 2000;
         final Thread wThread = new Thread( new PageWriter(COUNT, wikiPageHandler) );
         final PageReader r1 = new PageReader(wikiPageHandler);
         final PageReader r2 = new PageReader(wikiPageHandler);
+        final PageReader r3 = new PageReader(wikiPageHandler);
+        final PageReader r4 = new PageReader(wikiPageHandler);
         final Thread r1Thread = new Thread(r1);
         final Thread r2Thread = new Thread(r2);
+        final Thread r3Thread = new Thread(r3);
+        final Thread r4Thread = new Thread(r4);
         r1Thread.start();
         r2Thread.start();
+        r3Thread.start();
+        r4Thread.start();
         wThread.start();
         wThread.join();
         r1Thread.join();
         r2Thread.join();
-        Assert.assertEquals(COUNT, r1.getCounter() + r2.getCounter());
+        r3Thread.join();
+        r4Thread.join();
+        Assert.assertEquals(COUNT, r1.getCounter() + r2.getCounter() + r3.getCounter() + r4.getCounter());
     }
 
     class PageWriter implements Runnable {
@@ -63,6 +73,8 @@ public class BufferedWikiPageHandlerTest {
     class PageReader implements Runnable {
 
         private final BufferedWikiPageHandler handler;
+        private final Random random = new Random();
+
         private int counter;
 
         PageReader(BufferedWikiPageHandler handler) {
@@ -77,6 +89,11 @@ public class BufferedWikiPageHandlerTest {
         public void run() {
             counter = 0;
             while (handler.getPage(true) != BufferedWikiPageHandler.EOQ) {
+                try {
+                    Thread.sleep(0, random.nextInt(2) );
+                } catch (InterruptedException ie) {
+                    throw new IllegalStateException();
+                }
                 counter++;
             }
         }
