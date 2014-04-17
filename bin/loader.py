e91f8f780df9d818e6defa1f7e889958ae3b8f8c
Michele Mostarda <michele.mostarda@gmail.com>
2014-04-17 19:49:37 +0200

    Completed and fixed loader driver script.

--- bin/loader.py
+++ bin/loader.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 LATEST_DUMPS = 'http://dumps.wikimedia.org/enwiki/latest/'
 
@@ -8,17 +10,24 @@ MVN_HEAP_SIZE = '8g'
 LOADER = 'com.machinelinking.storage.DefaultJSONStorageLoader'
 LOADER_CONFIG = 'storage-loader.default.properties'
 
+
 import urllib
 import lxml
+import os
 import re
 import time
 import subprocess
 
+from lxml import html
+
+
 def atoi(text):
     return int(text) if text.isdigit() else text
 
+
 def natural_keys(text):
     return [ atoi(c) for c in re.split('(\d+)', text) ]
+
 
 def get_latest_articles_list():
     dumps_page = urllib.urlopen(LATEST_DUMPS).read()
@@ -28,8 +37,10 @@ def get_latest_articles_list():
     latest_download_links.sort(key=natural_keys)
     return latest_download_links
 
+
 def get_filename(url):
     return url.split('/')[-1].split('#')[0].split('?')[0]
+
 
 def download_file(url, dir, file):
         try:
@@ -42,11 +53,13 @@ def download_file(url, dir, file):
         except Exception as e:
             raise Exception("Error while downloading url [%s] in file [%s]" % (url, file), e)
 
+
 def ingest_file(config, file):
-    cmd = "MAVEN_OPTS='-Xms%s -Xmx%s -Dlog4j.configuration=file:conf/log4j.properties' %s exec:java -Dexec.mainClass=%s -Dexec.args='%s %s' 2>1 1> %s.log" \
+    cmd = "MAVEN_OPTS='-Xms%s -Xmx%s -Dlog4j.configuration=file:conf/log4j.properties' %s exec:java -Dexec.mainClass=%s -Dexec.args='%s %s' 2>&1 1> %s.log" \
           % (MVN_HEAP_SIZE, MVN_HEAP_SIZE, MVN_BIN, LOADER, config, file, file)
     print 'Executing command:', cmd
     subprocess.check_call(cmd, shell=True)
+
 
 def process_articles_dumps(config, n):
     latest_articles_links = get_latest_articles_list()
@@ -68,3 +81,15 @@ def process_articles_dumps(config, n):
         print 'Ingestion complete in %d sec.' % (it2 - it1)
         os.remove(article_file)
         print 'File deleted.'
+
+
+if __name__ == '__main__':
+    """
+    Example usage: bin/loader.py conf/storage-loader.default.properties 1
+    """
+    import sys
+    if len(sys.argv) != 3:
+        print 'Usage: $0 <config-file> <no-dumps>'
+        sys.exit(1)
+    process_articles_dumps(sys.argv[1], int(sys.argv[2]))
+    sys.exit(0)
