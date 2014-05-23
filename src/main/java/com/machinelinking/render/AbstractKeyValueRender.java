bfeac01f239af0ee4575f24fcd479647142a80c2
Michele Mostarda <michele.mostarda@gmail.com>
2014-05-23 18:38:30 +0200

    Removed escaping.

--- src/main/java/com/machinelinking/render/AbstractKeyValueRender.java
+++ src/main/java/com/machinelinking/render/AbstractKeyValueRender.java
@@ -13,7 +13,7 @@ public class AbstractKeyValueRender implements KeyValueRender {
     public void render(JsonContext context, RootRender rootRender, String key, JsonNode value, HTMLWriter writer)
     throws IOException {
         writer.openTag("p");
-        writer.text(value.asText());
+        writer.html(value.asText());
         writer.closeTag();
     }
 
