2a1a891cc3eb83bf17c0c4b4208b1b21a4d1b12f
Michele Mostarda <michele.mostarda@gmail.com>
2014-07-03 18:41:20 +0200

    Avoid deleting for every ingestion.

--- src/test/java/com/machinelinking/storage/AbstractJSONStorageLoaderTest.java
+++ src/test/java/com/machinelinking/storage/AbstractJSONStorageLoaderTest.java
@@ -24,7 +24,7 @@ public abstract class AbstractJSONStorageLoaderTest {
             WikiEnricherFactory.Validate
     };
 
-    private boolean cleanupDone = false;
+    private static boolean cleanupDone = false;
 
     protected abstract JSONStorage getJSONStorage() throws UnknownHostException;
 
