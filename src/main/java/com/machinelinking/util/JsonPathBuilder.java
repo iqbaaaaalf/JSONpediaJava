6b23e304c563391b4367436ec2ffbec5dabdbd9f
Michele Mostarda <michele.mostarda@gmail.com>
2014-08-18 18:29:19 +0200

    Added copyright.

--- src/main/java/com/machinelinking/util/JsonPathBuilder.java
+++ src/main/java/com/machinelinking/util/JsonPathBuilder.java
@@ -1,3 +1,16 @@
+/*
+ * JSONpedia - Convert any MediaWiki document to JSON.
+ *
+ * Written in 2014 by Michele Mostarda <mostarda@fbk.eu>.
+ *
+ * To the extent possible under law, the author has dedicated all copyright and related and
+ * neighboring rights to this software to the public domain worldwide.
+ * This software is distributed without any warranty.
+ *
+ * You should have received a copy of the CC BY Creative Commons Attribution 4.0 Internationa Public License.
+ * If not, see <https://creativecommons.org/licenses/by/4.0/legalcode>.
+ */
+
 package com.machinelinking.util;
 
 /**
