8e18302ae904d307133cf6ecc36d164c4158054e
Michele Mostarda <michele.mostarda@gmail.com>
2015-02-20 17:01:21 +0100

    Dropping main db too.

--- bin/drop_dbs.sh
+++ bin/drop_dbs.sh
@@ -11,6 +11,8 @@ echo DROP MongoDB? [Y/n]
 read r
 if [[ "Y" == "$r" ]]; then
 mongo --port $MONGO_PORT <<EOF
+use jsonpedia
+db.dropDatabase()
 use jsonpedia_test_db
 db.dropDatabase()
 use jsonpedia_loader_test
@@ -27,6 +29,8 @@ fi
 echo DROP Elastisearch? [Y/n]
 read r
 if [[ "Y" == "$r" ]]; then
+curl -XDELETE "http://localhost:$ELASTIC_PORT/jsonpedia/" -o -
+echo
 curl -XDELETE "http://localhost:$ELASTIC_PORT/jsonpedia_loader_test/" -o -
 echo
 curl -XDELETE "http://localhost:$ELASTIC_PORT/jsonpedia_test_facet/" -o -
